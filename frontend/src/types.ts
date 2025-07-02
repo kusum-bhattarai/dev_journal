77b887d386d2fa242bf31ca119bf86c18cd385ac
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2025-07-02 10:03:58 -0500

    Created type interfaces for messaging

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,22 @@
-export {}
+export interface User {
+    user_id: number;
+    username: string;
+    email: string;
+}
+
+export interface Conversation {
+    conversation_id: number;
+    user1_id: number;
+    user2_id: number;
+    last_message_id?: number;
+    created_at: string;
+}
+
+export interface Message {
+    message_id: number;
+    sender_id: number;
+    receiver_id: number;
+    content: string;
+    timestamp: string;
+    read_status: boolean;
+}
